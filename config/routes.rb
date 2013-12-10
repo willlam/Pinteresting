5f17a1c0ec91a3c4c2d4d78057f58452984ab50f
Will Lam <will@willlam.net>
2013-12-10 18:43:33 +0000

    created navbar in application.html.erb to be consistent across app

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Pinteresting::Application.routes.draw do
   root "pages#home"
-  get "about" => "pages#about"
+  get "about" => "pages#about" #creates about path
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
