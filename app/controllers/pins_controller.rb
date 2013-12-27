fed84c595b38077ac728e7b1765b4cc8bcbfd888
Will Lam <will@willlam.net>
2013-12-27 20:45:59 +0000

    fixed authentication by re-ordering before action in pin_controller

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,7 +1,8 @@
 class PinsController < ApplicationController
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
-  before_action :correct_user, only: [:edit, :update, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :correct_user, only: [:edit, :update, :destroy]
+
 
   def index
     @pins = Pin.all
@@ -46,7 +47,9 @@ class PinsController < ApplicationController
     end
   
     def correct_user
-      @pin = current_user.pins.find_by(id: params[:id])
+      # if current_user
+        @pin = current_user.pins.find_by(id: params[:id]);
+      # end
       redirect_to pins_path, notice: "Not authorized to edit this pin" if @pin.nil?
     end
 
