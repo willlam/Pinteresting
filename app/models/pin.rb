6a305cbf05634b94894c3c8e62cbac45f530c267
willlam <will@willlam.net>
2014-02-08 21:11:42 +0000

    Add names to users and validations for name, email and password

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,4 +2,8 @@ class Pin < ActiveRecord::Base
   belongs_to :user
   
   has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+  
+  validates :image, presence: true
+  validates :description, presence: true
+  validates :description, length: { minimum: 2 }
 end
